096b13e6c3935cdba4c0dd7a0079ad1ee072ee84
Slash-Tec <91566059+Slash-Tec@users.noreply.github.com>
2022-10-26 20:46:36 +0200

    Preparado para el primer exámen
    Plantilla para el exámen

--- app/views/header.php
+++ app/views/header.php
@@ -9,6 +9,9 @@
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
+    <!-- FontAwesome -->
+    <script src="https://kit.fontawesome.com/c858fc57f5.js" crossorigin="anonymous"></script>
+
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 
@@ -18,16 +21,56 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-    <a href="<?= ROOT ?>" class="navbar-brand">Tienda</a>
+    <a href="<?= ROOT ?>shop" class="navbar-brand">Tienda</a>
     <div class="collapse navbar-collapse" id="menu">
 <!--        Enlaces del menú para todos-->
         <?php if($data['menu']): ?>
-<!--            Ubicación del menú para usuarios logueados-->
+            <div class="d-flex justify-content-start">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a href="<?= ROOT ?>courses" class="nav-link <?= (isset($data['active']) && $data['active']=='courses') ? 'active' : '' ?>">Cursos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?= ROOT ?>books" class="nav-link <?= (isset($data['active']) && $data['active']=='books') ? 'active' : '' ?>">Libros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?= ROOT ?>shop/whoami" class="nav-link <?= (isset($data['active']) && $data['active']=='whoami') ? 'active' : '' ?>">Quienes somos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?= ROOT ?>shop/contact" class="nav-link <?= (isset($data['active']) && $data['active']=='contact') ? 'active' : '' ?>">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="d-flex justify-content-end">
+                <ul class="nav navbar-nav navbar-right">
+                    <?php if(isset($_SESSION['cartTotal']) && $_SESSION['cartTotal'] > 0): ?>
+                    <li class="nav-item">
+                        <a href="<?= ROOT ?>cart" class="nav-link">
+                            Carrito: <?= number_format($_SESSION['cartTotal'], 2) ?>&euro;
+                        </a>
+                    </li>
+                    <?php endif; ?>
+                    <li class="nav-item">
+                        <form action="<?= ROOT ?>search/products" class="d-flex" method="POST">
+                            <input type="text" name="search" id="search" class="form-control"
+                                   size="20" placeholder="¿producto?" required
+                            >
+                            <button type="submit" class="btn btn-light"><i class="fas fa-search"></i></button>
+                        </form>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?= ROOT ?>shop/logout" class="nav-link">Salir</a>
+                    </li>
+                </ul>
+            </div>
         <?php endif; ?>
         <?php if(isset($data['admin']) && $data['admin']): ?>
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                 <li class="nav-item">
                     <a href="<?= ROOT ?>adminuser" class="nav-link">Usuarios</a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?= ROOT ?>adminproduct" class="nav-link">Productos</a>
                 </li>
             </ul>
         <?php endif; ?>
